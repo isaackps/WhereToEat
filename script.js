8ef764bdd9319aae0f4286c846591e664101fc9d
Isaac Koh <isaackoh@Isaacs-MacBook-Pro.local>
2017-06-07 21:36:52 +0800

    random food generator

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 const button = document.getElementById('btn');
+const displayLocation = document.getElementById('ans');
+//const newDiv = document.createElement('div');
+let counter = 0;
+
+
 button.addEventListener('click', () => {
-  let x = getRandomInt(0, 9);
+  let x = getRandomInt(0, 11);
   let place;
+
+  if (counter = 0) {
     if (x < 4){
-      place = 
-    }else if (x > 3 && x < 7) {
-
-    }else if (x > 6 && x < 10) {
-
+      place = 'Shaw Food Republic';
+      displayLocation.append(place);
+      counter++;
+    } else if (x > 3 && x < 7) {
+      place = 'Orchard Towers';
+      displayLocation.append(place);
+      counter++;
+    } else if (x > 6 && x < 10) {
+      place = 'Tangs Food Market';
+      displayLocation.append(place);
+      counter++;
+    } else if (x == 10) {
+      place = 'SOMETHING EXPENSIVE! HANS TREAT!';
+      displayLocation.append(place);
+      counter++;
     }
+  } else {
+    if (x < 4){
+      place = 'Shaw Food Republic';
+      displayLocation.innerHTML = place;
+    } else if (x > 3 && x < 7) {
+      place = 'Orchard Towers';
+      displayLocation.innerHTML = place;
+    } else if (x > 6 && x < 10) {
+      place = 'Tangs Food Market';
+      displayLocation.innerHTML = place;
+    } else if (x == 10) {
+      place = 'SOMETHING EXPENSIVE! HANS TREAT!';
+      displayLocation.innerHTML = place;
+    }
+    //counter = 0;
+  }
+
 });
 
-getRandomInt(min, max) => {
+function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
